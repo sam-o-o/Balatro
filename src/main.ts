ce8ba79c2e09438b0f5c788d2f087ea78599728e
Hannes Höglund <hoglund.hannes@gmail.com>
2025-02-24 15:38:42 +0100

    Added cursed images plus more

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,32 @@ const sizes = {
     height: 600
 }
 
+class GameScene extends Phaser.Scene{
+    constructor(){
+        super("scene-game")
+    }
+
+    preload(){
+        this.load.image("bg", "/assets/bg.jpg")
+        this.load.image("all", "/assets/playing_cards/All.png")
+    }
+
+    create(){
+        this.add.image(0, 0, "bg").setOrigin(0, 0)
+        this.add.image(800, 500, "all").setOrigin(0, 0)
+    }
+
+    update(){}
+}
+
 const gameCanvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
 
 const config = {
     type:Phaser.WEBGL,
     width: sizes.width,
     height: sizes.height,
-    canvas: gameCanvas
+    canvas: gameCanvas,
+    scene:[GameScene]
 }
 
 const game = new Phaser.Game(config)
