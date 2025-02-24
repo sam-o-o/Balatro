1b7c58acc237446d9a7a3f9231028b21253a9eb5
Hannes Höglund <hoglund.hannes@gmail.com>
2025-02-24 14:48:04 +0100

    Created box

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
 import './style.css'
+import Phaser from 'phaser'
+
+const sizes = {
+    width: 900,
+    height: 600
+}
+
+const gameCanvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
+
+const config = {
+    type:Phaser.WEBGL,
+    width: sizes.width,
+    height: sizes.height,
+    canvas: gameCanvas
+}
+
+const game = new Phaser.Game(config)
